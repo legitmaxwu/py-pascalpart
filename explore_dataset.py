1609c489790d824a065fae666428ba5ad5f23d6f
Max Wu <chengmaxwu@gmail.com>
2019-12-23 01:58:27 -0800

    create segmentation maps

--- explore_dataset.py
+++ explore_dataset.py
@@ -2,16 +2,21 @@ import argparse
 import PIL
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 import utils
 
 SHOW_IMAGES = True
 
+MAP_PART_TO_DARKNESS = {}
+darkness_increment = 4
+darkness = 10
+
 # Plot a mask composed by 0s and 1s with a certain title
 # and compare it with the original image:
-def plot_mask(img, mask, bodypart_mask, windowtitle, suptitle):
-    mask = PIL.Image.fromarray(mask * 255)
-    bodypart_mask = PIL.Image.fromarray(bodypart_mask * 255)
+def plot_mask(img, mask, windowtitle, suptitle):
+    mask = PIL.Image.fromarray(mask)
+    # bodypart_mask = PIL.Image.fromarray(bodypart_mask * 255)
     fig = plt.figure()
     fig.canvas.set_window_title(windowtitle)
     fig.suptitle(suptitle)
@@ -21,10 +26,8 @@ def plot_mask(img, mask, bodypart_mask, windowtitle, suptitle):
     fig.add_subplot(1, 3, 2)
     plt.axis("off")
     plt.imshow(mask)
-    fig.add_subplot(1, 3, 3)
-    plt.axis("off")
-    plt.imshow(bodypart_mask)
     plt.show()
+    input()
 
 # Load annotations from the annotation folder of PASCAL-Part dataset:
 if __name__ == "__main__":
@@ -39,6 +42,7 @@ if __name__ == "__main__":
     bodypart_cnt = 0
 
     mat_filenames = os.listdir(args.annotation_folder)
+
     # Iterate through the .mat files contained in path:
     for idx, annotation_filename in enumerate(mat_filenames):
         annotations = utils.load_annotations(os.path.join(args.annotation_folder, annotation_filename))
@@ -48,11 +52,20 @@ if __name__ == "__main__":
 
         # Show original image with its mask:
         img = PIL.Image.open(os.path.join(args.images_folder, image_filename))
+        img_width, img_height = img.size
+        total_mask = np.zeros([img_height, img_width])
         for obj in annotations["objects"]:
             bodypart_cnt += len(obj["parts"])
             print("obj_cnt: {} - bodypart_cnt: {}".format(obj_cnt, bodypart_cnt), end="\r")
             if SHOW_IMAGES:
                 for body_part in obj["parts"]:
-                    plot_mask(img, obj["mask"], body_part["mask"], image_filename, obj["class"] + ": " + body_part["part_name"])
+                    if body_part['part_name'] not in MAP_PART_TO_DARKNESS:
+                        MAP_PART_TO_DARKNESS[body_part['part_name']] = darkness
+                        darkness += darkness_increment
+                    total_mask = np.maximum(total_mask, body_part["mask"] * MAP_PART_TO_DARKNESS[body_part['part_name']])
+        
+        print(MAP_PART_TO_DARKNESS)
+        plot_mask(img, total_mask, image_filename, obj["class"] + ": " + body_part["part_name"])
+
 
     print("obj_cnt: {} - bodypart_cnt: {}".format(obj_cnt, bodypart_cnt))
